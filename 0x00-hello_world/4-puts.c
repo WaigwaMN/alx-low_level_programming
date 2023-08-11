65c312a7726bd3390edad49416eb4057a41f3742
WaigwaMN <mwaigwao77@gmail.com>
2023-08-11 04:27:09 -0700

    number 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: returns 0 when run
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

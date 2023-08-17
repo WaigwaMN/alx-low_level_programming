fa8b1f8a0198aefd94e63d874a4c98d82476008a
WaigwaMN <mwaigwao77@gmail.com>
2023-08-17 04:40:49 -0700

    a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main code function
+ *
+ * Return: always zero
+ */
+
+int main(void)
+{
+	char i
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}

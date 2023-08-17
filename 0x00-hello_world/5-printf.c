53b2a1a6dccbd1dda19f2a9753e2ea8a61f21d60
WaigwaMN <mwaigwao77@gmail.com>
2023-08-17 01:24:01 -0700

    grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this the main function
+ *
+ * Return: always 0 when the code runs successfully
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

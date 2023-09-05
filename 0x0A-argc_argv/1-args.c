043eb027c812800ad73d8624262aee9a785b8abd
WaigwaMN <mwaigwao77@gmail.com>
2023-09-05 13:40:35 -0700

    silence is argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument
+ * @argv: argument
+ * Return: 0 when succesful
+ * */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

24e0a89e240f84c17d4a919e1ba568306338ccd5
WaigwaMN <mwaigwao77@gmail.com>
2023-09-07 02:10:45 -0700

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - .
+ * @s1: .
+ * @s2: .
+ * @n: .
+ * Return: .
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/**
+ * string_nconcat - .
+ * @s1: .
+ * @s2: .
+ * @n: .
+ * Description: .
+ * Return: .
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+
+{
+	unsigned int index, len1, len2;
+	char *string;
+
+	if (s2 == NULL)
+		s2 = "";
+
+	if (s1 == NULL)
+		s1 = "";
+
+	len1 = 0;
+	len2 = 0;
+
+	while (s2[len2] != '\0')
+		len2++;
+	while (s1[len1] != '\0')
+		len1++;
+
+	if (n >= len2)
+		n = len2;
+
+	string = malloc(sizeof(char) * n + len1 + 1);
+
+	if (string == NULL)
+		return (NULL);
+
+	for (index = 0; index < len1; index++)
+		string[index] = s1[index];
+
+	for (index = 0; index < n; index++)
+		string[index + len1] = s2[index];
+
+	string[index + len1] = '\0';
+
+	return (string);
+}
